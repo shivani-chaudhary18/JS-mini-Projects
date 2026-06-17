a907374f7763fb424f3a8111032f56c8a8141c06
shivani chaudhary <144934894+shivani-chaudhary18@users.noreply.github.com>
2026-06-17 21:42:35 +0530

    Add files via upload

---
+++ incORdec/script.js
@@ -0,0 +1,15 @@
+let h1 = document.querySelector("h1");
+let inc = document.querySelector("#inc");
+let dec = document.querySelector("#dec");
+
+let a = 0;
+
+inc.addEventListener("click",()=>{
+      a++;
+      h1.innerHTML = a;
+})
+
+dec.addEventListener("click",()=>{
+      a--;
+      h1.innerHTML = a;
+})
